d9555ad689775c6a0c2fbfc6cbbc89008024e04f
Z1V3 <azivko@foi.hr>
2023-12-24 00:48:33 +0100

    Implemented logic for adding new cards (v0.1)

--- Android/lib/pages/add_card_page.dart
+++ Android/lib/pages/add_card_page.dart
@@ -1,9 +1,8 @@
+import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
 import 'dart:async';
 import 'dart:convert';
-import 'package:android/pages/charging_info.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:intl/intl.dart';
 import 'package:android/privateAddress.dart';
 import 'package:android/providers/user_provider.dart';
 import 'package:provider/provider.dart';
@@ -19,18 +18,82 @@ class AddCardPage extends StatefulWidget {
 
 class _ChargeModePageState extends State<AddCardPage>{
 
+  NFCTag? _tag;
+  String _result = "";
+  bool _readCard = false;
+  int _resultStart = 1;
+
   @override
-  void initState() {
+  void initState(){
     super.initState();
 
     startNFC();
+    if(_resultStart == 0){
+      readNFC();
+      if(_readCard == true){
+        sendAddNewCard(_result);
+      }
+    }
   }
 
-  void startNFC() {
+  void startNFC() async {
     print('Please scan your card!');
+    bool errorChecker = false;
+
+    try {
+      final NFCTag tag = await FlutterNfcKit.poll();
+      setState(() {
+        _tag = tag;
+      });
+
+      if (tag.type == NFCTagType.iso18092) {
+        String result1 =
+            await FlutterNfcKit.transceive("060080080100");
+        setState(() {
+          _result = '1: $result1\n';
+        });
+      } else if (tag.ndefAvailable ?? false) {
+        var ndefRecords = await FlutterNfcKit.readNDEFRecords();
+        var ndefString = '';
+        for (int i = 0; i < ndefRecords.length; i++) {
+          ndefString += '${i + 1}: ${ndefRecords[i]}\n';
+        }
+        setState(() {
+          _result = ndefString;
+        });
+      } else if (tag.type == NFCTagType.webusb) {
+        await FlutterNfcKit.transceive(
+            "00A4040006D27600012401");
+      }
+
+    } catch (e) {
+      setState(() {
+        _result = 'error: $e';
+        errorChecker = true;
+      });
+    }
+    if(errorChecker == true){
+      _resultStart = 1;
+    }else{
+      _resultStart = 0;
+    }
   }
 
-  Future<void> sendAddNewCard(int userID, String cardValue) async {
+  void readNFC() async{
+    var ndefRecords = await FlutterNfcKit.readNDEFRecords();
+    var ndefString = '';
+    for (int i = 0; i < ndefRecords.length; i++) {
+      ndefString += '${i + 1}: ${ndefRecords[i]}\n';
+    }
+    setState(() {
+      _readCard = true;
+      _result = ndefString;
+    });
+
+    await Navigator.pushReplacementNamed(context, 'userModePageRoute');
+  }
+
+  Future<void> sendAddNewCard(String cardValue) async {
     final Uri uri = Uri.parse('http://${returnAddress()}:8080/api/card/addNewCard');
 
     int userID = Provider.of<UserProvider>(context, listen: false).user?.userID ?? 0;
