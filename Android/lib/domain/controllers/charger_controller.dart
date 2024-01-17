e855febcacf13a8dafd8a5eabdc92b7e4de121f2
Z1V3 <azivko@foi.hr>
2024-01-17 12:36:08 +0100

    Added charger_controller that will be used in the page for calling the functions and then executing them with the use_cases

---
+++ Android/lib/domain/controllers/charger_controller.dart
@@ -0,0 +1,19 @@
+import 'package:android/domain/use_cases/update_charger_availability.dart';
+import 'package:android/domain/use_cases/create_charging_event.dart';
+import 'dart:async';
+
+class ChargeController {
+  final UpdateChargerAvailability _updateChargerAvailability;
+  final CreateChargingEvent _createChargingEvent;
+
+  ChargeController(this._updateChargerAvailability, this._createChargingEvent);
+
+  Future<void> updateChargerAvailability(int chargerID, bool occupied) async {
+    await _updateChargerAvailability.execute(chargerID, occupied);
+  }
+
+  Future<void> createChargingEvent(String startTime, String endTime, String chargeTime, double volume, double price) async {
+    await _createChargingEvent.execute(startTime, endTime, chargeTime, volume, price);
+  }
+
+}
