ff12cee221abfb1320160a0b4693ac76da41519f
Luka Kraljic <lkraljic1@student.foi.hr>
2024-01-18 22:53:10 +0100

    Add: google maps & markers

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
-import './App.css';
-import AddChargerModal from './components/AddEditChargerModal/AddChargerModal';
+import React, { useState } from "react";
+import "./App.css";
+import AddChargerModal from "./components/AddEditChargerModal/AddChargerModal";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Sample/Navbar";
+import Map from "./Sample/Map";
 
 function App() {
   const [isAddChargerModalOpen, setIsAddChargerModalOpen] = useState(false);
@@ -14,16 +17,22 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={openAddChargerModal}>Add charging station</button>
-        {isAddChargerModalOpen && (
-          <div className="modal-overlay">
-            <AddChargerModal onClose={closeAddChargerModal} />
-          </div>
-        )}
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <header className="App-header">
+          <button onClick={openAddChargerModal}>Add charging station</button>
+          {isAddChargerModalOpen && (
+            <div className="modal-overlay">
+              <AddChargerModal onClose={closeAddChargerModal} />
+            </div>
+          )}
+        </header>
+        <div className="map-container">
+          <Map />
+        </div>
+      </div>
+    </Router>
   );
 }
 
