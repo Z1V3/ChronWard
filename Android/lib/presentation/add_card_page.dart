6d4963ffd06ee5fe7e8b83d12c2be1ea94f168fd
Z1V3 <azivko@foi.hr>
2024-02-12 11:23:32 +0100

    Design improvements on Add card page and Charge mode page

--- Android/lib/presentation/add_card_page.dart
+++ Android/lib/presentation/add_card_page.dart
@@ -1,9 +1,11 @@
-import 'package:android/domain/controllers/card_controller.dart';
-import 'package:android/presentation/widgets/drawer_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:core/handlers/nfc_handler.dart';
+import 'package:android/domain/controllers/card_controller.dart';
 import 'package:ws/services/card_service.dart';
 import 'package:android/domain/use_cases/add_card.dart';
+import 'package:android/presentation/widgets/drawer_widget.dart';
+import 'package:provider/provider.dart';
+import 'package:core/providers/user_provider.dart';
 
 class AddCardPage extends StatefulWidget {
   const AddCardPage({Key? key}) : super(key: key);
@@ -14,27 +16,44 @@ class AddCardPage extends StatefulWidget {
 
 class _AddCardPageState extends State<AddCardPage> {
   late CardController _cardController;
+  bool _isLoading = true; // Set initial loading state to true
 
   @override
   void initState() {
     super.initState();
     _cardController = CardController(AddCard(CardService()));
     NFCHandler.startNFCReading(
-            (hexIdentifier) {
-          debugPrint("Received NFC Identifier (Hex): $hexIdentifier");
-          _cardController.sendAddNewCard(hexIdentifier);
-        },
-            (errorMessage) {
-          debugPrint("Error occurred while reading NFC: $errorMessage");
-          // Handle the error
-        }
+          (hexIdentifier) {
+        setState(() {
+          _isLoading = false; // Update loading state when NFC reading is complete
+        });
+        debugPrint("Received NFC Identifier (Hex): $hexIdentifier");
+        int userID = Provider.of<UserProvider>(context, listen: false).user?.userID ?? 0;
+        _cardController.sendAddNewCard(userID, hexIdentifier);
+        // Show Snackbar when the card is added successfully
+        _showSnackbar(context, 'Card added successfully');
+        Navigator.pushReplacementNamed(context, 'userModePageRoute');
+      },
+          (errorMessage) {
+        setState(() {
+          _isLoading = false; // Update loading state when NFC reading encounters an error
+        });
+        debugPrint("Error occurred while reading NFC: $errorMessage");
+        // Handle the error
+      },
     );
+  }
 
+  void _showSnackbar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Charge Mode'),
@@ -43,28 +62,29 @@ class _AddCardPageState extends State<AddCardPage> {
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pushReplacementNamed(context, 'userModePageRoute');
-            },
+          },
         ),
       ),
       drawer: const DrawerWidget(),
-      body: PopScope(
-        child: Container(
-          color: Colors.black87,
-          child: const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 40),
-                Text(
-                  'Please scan your card...',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: Container(
+        color: Colors.black87,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (_isLoading)
+                const CircularProgressIndicator() // Show loading indicator if isLoading is true
+              else
+                const SizedBox(height: 40), // Empty SizedBox if not loading
+              const Text(
+                'Please scan your card...',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- Android/lib/presentation/charge_mode_page.dart
+++ Android/lib/presentation/charge_mode_page.dart
@@ -2,13 +2,11 @@ import 'dart:async';
 import 'package:android/domain/use_cases/create_charging_event.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:core/providers/user_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:ws/models/charging_data.dart';
 import 'package:core/utils/duration_formatter.dart';
 import 'package:android/domain/controllers/charger_controller.dart';
 import 'package:ws/services/charge_service.dart';
 import 'package:android/domain/use_cases/update_charger_availability.dart';
-
 
 class ChargeModePage extends StatefulWidget {
   const ChargeModePage({Key? key}) : super(key: key);
@@ -19,6 +17,15 @@ class ChargeModePage extends StatefulWidget {
 
 class _ChargeModePageState extends State<ChargeModePage>{
   late ChargeController _chargeController;
+  final ChargingData _chargingData = ChargingData(startTime: "/", endTime: "/", chargeTime: "/", volume: 0, price: 0, userID: 0, chargerID: 0);
+
+  late Timer _timer;
+  bool isRunning = false;
+  double counter = 0.00;
+  double loadingProgress = 0.0; // Added loading progress
+
+  bool startButtonVisible = true;
+  bool stopButtonVisible = false;
 
   @override
   void initState() {
@@ -26,15 +33,6 @@ class _ChargeModePageState extends State<ChargeModePage>{
     _chargeController = ChargeController(UpdateChargerAvailability(ChargeService()),
         CreateChargingEvent(ChargeService()));
   }
-
-  late Timer _timer;
-  bool isRunning = false;
-
-  double counter = 0.00;
-  double volumeCharge = 0.00, priceCharge = 0.00;
-
-  String formattedDateTimeStart = "/", formattedDateTimeEnd = "/", formattedDuration = "/";
-  int globalUserID = 0;   // This is being outputted to the user so in the production version make it not be hardcoded or don't log it at all
 
   @override
   void dispose() {
@@ -46,6 +44,9 @@ class _ChargeModePageState extends State<ChargeModePage>{
     _timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
       setState(() {
         counter++;
+        loadingProgress = counter / 300; // 30 seconds -> 300 ticks
+        // Update charging session information while running
+        _updateChargingData();
       });
     });
   }
@@ -62,37 +63,104 @@ class _ChargeModePageState extends State<ChargeModePage>{
       startTimer();
       setState(() {
         isRunning = true;
+        startButtonVisible = false;
+        stopButtonVisible = true;
       });
       _chargeController.updateChargerAvailability(1, true);
       DateTime now = DateTime.now();
-      formattedDateTimeStart = DateFormat("yyyy-MM-ddTHH:mm:ss").format(now);
+      _chargingData.startTime = DateFormat("yyyy-MM-ddTHH:mm:ss").format(now);
     }
-    print('START button clicked');
+    debugPrint('START button clicked');
   }
 
-  void onPressStop() {
+  void onPressStop(BuildContext context) {
     if (isRunning) {
       stopTimer();
 
       DateTime now = DateTime.now();
-      formattedDateTimeEnd = DateFormat("yyyy-MM-ddTHH:mm:ss").format(now);
+      _chargingData.endTime = DateFormat("yyyy-MM-ddTHH:mm:ss").format(now);
 
       counter = counter/10;
       int unformattedDuration = counter.toInt();
-      formattedDuration = DurationFormatter.formatCounterToDuration(unformattedDuration);
 
-      volumeCharge = double.parse(((counter*2.361)/10).toStringAsExponential(3));
-      priceCharge = double.parse(((counter*1.5)/10).toStringAsExponential(3));
-
+      _chargingData.chargeTime = DurationFormatter.formatCounterToDuration(unformattedDuration);
+      _chargingData.volume = _calculateVolume(counter);
+      _chargingData.price = _calculatePrice(counter);
+      _chargingData.userID = 1;
+      _chargingData.chargerID = 1;
 
       _chargeController.updateChargerAvailability(1, false);
-      _chargeController.createChargingEvent(formattedDateTimeStart, formattedDateTimeEnd, formattedDuration, volumeCharge, priceCharge);
+      _chargeController.createChargingEvent(_chargingData.startTime, _chargingData.endTime, _chargingData.chargeTime, _chargingData.volume, _chargingData.price);
+
+      // Show pop-up message with charging data
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Charging Session Ended'),
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('UserID: ${_chargingData.userID}',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Bahnschrift', // or any other desired font
+                  ),
+                ),
+                Text('Time charged: ${_chargingData.chargeTime}',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Bahnschrift', // or any other desired font
+                  ),
+                ),
+                Text('Power: ${(_chargingData.volume).toStringAsFixed(3)} kWh',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Bahnschrift', // or any other desired font
+                  ),
+                ),
+                Text('Price: ${(_chargingData.price).toStringAsFixed(3)} EUR',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Bahnschrift', // or any other desired font
+                  ),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
     }
-    print('UserID: $globalUserID');
-    print('Time charged: $counter seconds');
-    print('Power: $volumeCharge');
-    print('Price: $priceCharge');
-    counter = 0;
+    setState(() {
+      counter = 0;
+      loadingProgress = 0;
+      startButtonVisible = true;
+      stopButtonVisible = false;
+    });
+  }
+  // Update charging data information
+  void _updateChargingData() {
+    _chargingData.volume = _calculateVolume(counter);
+    _chargingData.price = _calculatePrice(counter);
+  }
+
+  // Calculate volume
+  double _calculateVolume(double counter) {
+    return double.parse(((counter * 2.361) / 10).toStringAsExponential(3));
+  }
+
+  // Calculate price
+  double _calculatePrice(double counter) {
+    return double.parse(((counter * 1.5) / 10).toStringAsExponential(3));
   }
 
   @override
@@ -105,43 +173,90 @@ class _ChargeModePageState extends State<ChargeModePage>{
     return Scaffold(
       appBar: AppBar(
         title: const Text('Charge Mode'),
-        backgroundColor: Colors.lightBlue[100],
+        backgroundColor: Colors.white,
       ),
       body: WillPopScope(
         onWillPop: onWillPop,
         child: Container(
-          color: Colors.black87,
+          color: Colors.blueAccent,
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-              const SizedBox(height: 40),
-              const Text(
-                //'Welcome, ${widget.userName}', // Display the welcome message with the user's name
-                'Welcome, Ivan Horvat',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 40),
-              Expanded(child: Center(
-                child: Column (
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CircleButton(
-                        label: 'START',
-                        onClick: !isRunning ? () => onPressStart() : null,
-                      ),
-                      const SizedBox(height: 20),
-                      CircleButton(
-                        label: 'STOP',
-                        onClick: isRunning ? () => onPressStop() : null,
-                      ),
-                    ],
+                const SizedBox(height: 40),
+                Expanded(
+                  child: Center(
+                    child: Column (
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Display charging session information
+                        isRunning ?
+                        Column(
+                          children: [
+                            Text(
+                              'Time Elapsed: ${Duration(milliseconds: (counter * 100).toInt()).inMinutes}:${Duration(milliseconds: (counter * 100).toInt()).inSeconds.remainder(60)} seconds',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              'Power: ${(_chargingData.volume / 10).toStringAsFixed(3)} kWh',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              'Price: ${(_chargingData.price / 10).toStringAsFixed(3)} EUR',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                              child: Container(
+                                height: 20,
+                                width: 200,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.grey[300],
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10), // Rounded corners for the clip
+                                  child: LinearProgressIndicator(
+                                    value: loadingProgress,
+                                    backgroundColor: Colors.transparent,
+                                    valueColor: const AlwaysStoppedAnimation<Color>(Colors.greenAccent),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ) :
+                        const SizedBox.shrink(),
+                        const SizedBox(height: 20),
+                        startButtonVisible ?
+                        CircleButton(
+                          color: Colors.yellow,
+                          icon: Icons.lightbulb,
+                          breathing: false,
+                          onClick: !isRunning ? () => onPressStart() : null,
+                        ) :
+                        const SizedBox.shrink(),
+                        const SizedBox(height: 20),
+                        stopButtonVisible ?
+                        CircleButton(
+                          color: Colors.redAccent,
+                          icon: Icons.stop_rounded,
+                          breathing: true,
+                          onClick: isRunning ? () => onPressStop(context) : null,
+                        ) :
+                        const SizedBox.shrink(),
+                      ],
+                    ),
                   ),
-                ),
                 ),
               ],
             ),
@@ -151,31 +266,95 @@ class _ChargeModePageState extends State<ChargeModePage>{
     );
   }
 }
-
-class CircleButton extends StatelessWidget {
-  final String label;
+class CircleButton extends StatefulWidget {
+  final IconData icon;
   final VoidCallback? onClick;
+  final Color color;
+  final bool breathing; // Added boolean to control breathing effect
 
-  const CircleButton({super.key, required this.label, this.onClick});
+  const CircleButton({Key? key, required this.icon, required this.color, required this.breathing, this.onClick}) : super(key: key);
+
+  @override
+  _CircleButtonState createState() => _CircleButtonState();
+}
+
+class _CircleButtonState extends State<CircleButton> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    _animation = Tween<double>(begin: 1.0, end: 1.2).animate(_controller);
+    if (widget.breathing) {
+      _controller.repeat(reverse: true);
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant CircleButton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.breathing && !_controller.isAnimating) {
+      _controller.repeat(reverse: true);
+    } else if (!widget.breathing && _controller.isAnimating) {
+      _controller.stop();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onClick,
-      style: ElevatedButton.styleFrom(
-        shape: const CircleBorder(),
-        padding: const EdgeInsets.all(30),
-        backgroundColor: Colors.blue,
-        minimumSize: const Size(200, 200),
-      ),
-      child: Text(
-        label,
-        style: const TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-        ),
-      ),
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: widget.breathing ? _animation.value : 1.0, // Apply scale only when breathing
+          child: ElevatedButton(
+            onPressed: widget.onClick,
+            style: ElevatedButton.styleFrom(
+              shape: const CircleBorder(),
+              padding: const EdgeInsets.all(30),
+              backgroundColor: widget.color,
+              minimumSize: const Size(200, 200),
+              shadowColor: widget.color.withOpacity(0.5), // Adding a shadow effect
+              elevation: 10, // Adding elevation for a 3D effect
+            ),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: RadialGradient(
+                      colors: [
+                        Colors.yellow.withOpacity(0.8), // Lighter yellow at the center
+                        Colors.orange.withOpacity(0.9), // Darker yellow at the edges
+                      ],
+                      stops: const [0.4, 1],
+                    ),
+                  ),
+                ),
+                Icon(
+                  widget.icon,
+                  color: Colors.white,
+                  size: 60,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
